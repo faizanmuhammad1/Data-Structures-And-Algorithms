344387960a00217d28cad72422443fec43942552
Faizan Muhammad <faizanmuhammad6412@gmail.com>
2023-12-12 16:03:41 +0500

    CHANGES

--- src/SortingAlgorithms/QuickSort.java
+++ src/SortingAlgorithms/QuickSort.java
@@ -1,7 +1,39 @@
 package SortingAlgorithms;
 
 public class QuickSort {
-    public void sort(int[] arr){
+    public static void main(String[] args) {
+        int[] array = {5, 3, 8, 4, 2};
+        quickSort(array, 0, array.length - 1);
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
 
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
+
